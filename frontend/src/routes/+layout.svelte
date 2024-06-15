368a864ad5f3142d56210471ab269381f174ccb6
Greg Holmes <git@greg.holmes.name>
2024-06-16 00:07:21 +0100

    Add Matcha

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,15 @@
-<nav>
-  <a href="/">Dashboard</a>
-</nav>
+<script>
+  import "@lowlighter/matcha/dist/matcha.css";
+  import "@lowlighter/matcha/dist/matcha.utilities.css";
+</script>
 
+<header>
+<nav>
+<menu>
+  <li><a href="/">Dashboard</a></li>
+</menu>
+</nav>
+</header>
+<main>
 <slot></slot>
+</main>
