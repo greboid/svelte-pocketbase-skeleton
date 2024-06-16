68459e59bae6310b1a53334f59c30f00d828ed69
Greg Holmes <git@greg.holmes.name>
2024-06-16 15:49:22 +0100

    Add fallback to index to get SK 404s

--- backend/main.go
+++ backend/main.go
@@ -14,7 +14,7 @@ func main() {
 
 	// serves static files from the provided public dir (if exists)
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
-		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
+		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), true))
 		return nil
 	})
 
